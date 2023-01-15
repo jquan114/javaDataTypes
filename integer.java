a3ce278ac1c683182f7cf6aa5625f85d9f56133e
JQUAN COLLINS <104362301+jquan114@users.noreply.github.com>
2023-01-15 02:31:12 -0500

    Create integer.java

---
+++ integer.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello world!");
+
+        //create variables
+
+        int s = 10;
+
+        //print the variable
+
+        System.out.println("s = "  +  s );
+    }
+}
